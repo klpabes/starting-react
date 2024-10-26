8cce8f5d9b931a27749ee8542f248cc99fe70447
klpabes <klpabes99@gmail.com>
2024-10-27 00:26:43 +0800

    css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import styled from "@emotion/styled";
 // import pokemon from "./pokemon.json";
 
 const PokemonRow = ({ pokemon, onSelect }) => (
@@ -55,6 +56,28 @@ PokemonInfo.propTypes = {
   }),
 };
 
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1rem;
+`;
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+  padding-top: 1rem;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+
 function App() {
   const [filter, filterSet] = useState("");
   const [pokemon, pokemonSet] = useState([]);
@@ -67,23 +90,11 @@ function App() {
   }, []);
 
   return (
-    <div
-      style={{
-        margin: "auto",
-        width: 800,
-        paddingTop: "1rem",
-      }}
-    >
-      <h1 className="title">Pokemon Search</h1>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "70% 30%",
-          gridColumnGap: "1rem",
-        }}
-      >
+    <Container>
+      <Title>Pokemon Search</Title>
+      <TwoColumnLayout>
         <div>
-          <input
+          <Input
             type="text"
             value={filter}
             onChange={(event) => filterSet(event.target.value)}
@@ -115,8 +126,8 @@ function App() {
           </table>
         </div>
         {selectedItem && <PokemonInfo {...selectedItem} />}
-      </div>
-    </div>
+      </TwoColumnLayout>
+    </Container>
   );
 }
 
